57b50d770c3e1c65e19d0af2facaf69978e6320b
anisb <anis.belharet@etu.univ-amu.fr>
2024-04-24 23:13:26 +0200

    Doc class Enigme

--- src/enigmes/Enigme.java
+++ src/enigmes/Enigme.java
@@ -10,28 +10,55 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-
+/**
+ * Cette classe représente une énigme avec une question et une réponse.
+ */
 public class Enigme {
     private String question;
     private String reponse;
-
+    /**
+     * Constructeur de la classe Enigme.
+     *
+     * @param question La question de l'énigme.
+     * @param reponse  La réponse de l'énigme.
+     */
     public Enigme(String question, String reponse) {
         this.question = question;
         this.reponse = reponse;
     }
-
+    /**
+     * Obtient la question de l'énigme.
+     *
+     * @return La question de l'énigme.
+     */
     public String getQuestion() {
         return question;
     }
-
+    /**
+     * Obtient la réponse de l'énigme.
+     *
+     * @return La réponse de l'énigme.
+     */
     public String getReponse() {
         return reponse;
     }
-
+    /**
+     * Vérifie si la réponse donnée par le joueur est correcte.
+     *
+     * @param reponseJoueur La réponse donnée par le joueur.
+     * @return true si la réponse est correcte, sinon false.
+     */
     public boolean verifierReponse(String reponseJoueur) {
         return reponseJoueur.equalsIgnoreCase(reponse);
     }
-
+    /**
+     * Charge les énigmes à partir d'un fichier JSON.
+     *
+     * @param nomFichier Le nom du fichier JSON contenant les énigmes.
+     * @return Une liste d'énigmes.
+     * @throws IOException   En cas d'erreur d'entrée/sortie lors de la lecture du fichier.
+     * @throws JSONException En cas d'erreur de manipulation JSON.
+     */
     public static List<Enigme> chargerEnigmesDepuisJSON(String nomFichier) throws IOException, JSONException, JSONException {
         List<Enigme> enigmes = new ArrayList<>();
         String contenuFichier = new String(Files.readAllBytes(Paths.get(nomFichier)));
@@ -44,7 +71,12 @@ public class Enigme {
         }
         return enigmes;
     }
-
+    /**
+     * Choisi une énigme aléatoire parmi une liste d'énigmes.
+     *
+     * @param enigmes La liste d'énigmes parmi lesquelles choisir.
+     * @return Une énigme choisie aléatoirement.
+     */
     public static Enigme choisirEnigmeAleatoire(List<Enigme> enigmes) {
         Random rand = new Random();
         return enigmes.get(rand.nextInt(enigmes.size()));
