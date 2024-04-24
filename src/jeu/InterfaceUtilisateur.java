b31adc372a0bfe7bb31d7a753cbbbc22f42f012a
anisb <anis.belharet@etu.univ-amu.fr>
2024-04-25 00:46:22 +0200

    DocInterface interface

--- src/jeu/InterfaceUtilisateur.java
+++ src/jeu/InterfaceUtilisateur.java
@@ -2,14 +2,32 @@ package jeu;
 import org.json.JSONException;
 
 import java.util.Scanner;
-
+ /**
+ * Cette classe gère l'interface utilisateur pour la création de compte et la connexion au jeu.
+ */
 public class InterfaceUtilisateur {
     private GestionCompte gestionCompte;
 
     public InterfaceUtilisateur() {
         this.gestionCompte = new GestionCompte();
     }
-
+    /**
+     * Lance le jeu.
+     *
+     * <p>Cette méthode permet à l'utilisateur de se connecter ou de créer un nouveau compte
+     * afin de démarrer le jeu. Elle guide l'utilisateur tout au long du processus et
+     * assure que les informations fournies sont valides.</p>
+     *
+     * <p>Si l'utilisateur choisit de se connecter (en saisissant "c"),
+     * il sera invité à entrer son nom d'utilisateur et son mot de passe. S'il fournit
+     * des informations valides, il pourra accéder au jeu.</p>
+     *
+     * <p>Si l'utilisateur choisit de créer un nouveau compte (en saisissant "n"),
+     * il lui sera demandé de fournir un nom d'utilisateur et un mot de passe. Un nouveau compte
+     * sera créé et il pourra ensuite s'y connecter.</p>
+     *
+     * @throws JSONException En cas d'erreur de manipulation JSON.
+     */
     public void demarrerJeu() throws JSONException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Bienvenue au jeu !");
