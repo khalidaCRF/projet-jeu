21ea5bee84dc89c99c7545fcfc702c482ae56b37
khcherifi <khalida.cherifi@soprasteria.com>
2024-04-26 19:31:03 +0200

    les nouvelles images

--- src/jeu/AudioPlayer.java
+++ src/jeu/AudioPlayer.java
@@ -1,3 +1,4 @@
+/*
 package jeu;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -21,4 +22,34 @@ public class AudioPlayer {
             e.printStackTrace();
         }
     }
+}
+*/
+package jeu;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+
+public class AudioPlayer {
+
+    public static void jouerSon(String nomFichier) {
+        try {
+            // Charger le fichier audio
+            File fichierAudio = new File("jeu/BlueBoyAdventure.wav/" + nomFichier);
+            AudioInputStream audioInput = AudioSystem.getAudioInputStream(fichierAudio);
+
+            // Créer un clip audio
+            Clip clip = AudioSystem.getClip();
+
+            // Ouvrir le fichier audio dans le clip
+            clip.open(audioInput);
+
+            // Jouer le clip
+            clip.start();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    // Utilisation : AudioPlayer.jouerSon("nom_de_votre_fichier.wav");
 }
--- src/jeu/Main.java
+++ src/jeu/Main.java
@@ -9,6 +9,6 @@ public class Main {
 		Jeu jeu = new Jeu();
 		GUI gui = new GUI( jeu);
 		jeu.setGUI( gui);
-		gui.afficherInterfaceConnexion();
+		//gui.afficherInterfaceConnexion();
 	}
 }
